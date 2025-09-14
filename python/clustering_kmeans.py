1654614dc83ec79f1b33ca6cbcc1659c84cbe4a5
Najib Mahjoubi <najib.mahjoubi@gmail.com>
2025-09-14 18:04:37 -0400

    gestion de la colonne target

--- python/clustering_kmeans.py
+++ python/clustering_kmeans.py
@@ -2,12 +2,15 @@ import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
+features_columns = dataset.columns.tolist()
+features_columns.remove('target_name')
+
 # Standardisation of the dataset
 scaler = StandardScaler()
-scaled_features = scaler.fit_transform(dataset)
+scaled_features = scaler.fit_transform(dataset[features_columns])
 
 # Convert the scaled array back to a DataFrame with original column names
-scaled_df = pd.DataFrame(scaled_features, columns=dataset.columns)
+scaled_df = pd.DataFrame(scaled_features, columns=features_columns)
 
 #  Clustering with KMeans
 kmeans = KMeans(n_clusters=3, random_state=42)
